38669c6af800cd2919b6bfe47e4d65dfc907917a
loren <lorenmichaelh@gmail.com>
2022-11-02 15:12:56 -0500

    commit tree 860f190bfaf15d9d90c88322a52bd40117674586

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,15 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+      
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def students
+    students = Student.all
+    render json: students
+  end
 
 end
